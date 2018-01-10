23f8465ab9ff2b1590021d18cc58987444dd044e
Filip3314 <fmusial@outlook.com>
2018-01-10 18:42:17 -0500

    Changed Motion Profile

--- src/org/usfirst/frc/team3314/robot/autos/MotionProfile.java
+++ src/org/usfirst/frc/team3314/robot/autos/MotionProfile.java
@@ -36,7 +36,6 @@ public class MotionProfile implements Autonomous {
      EncoderFollower right = new EncoderFollower(modifier.getRightTrajectory());
      
      motionProfileStates currentState;
-     motionProfileStates nextState;
      
      double time = 0;
      
@@ -45,57 +44,35 @@ public class MotionProfile implements Autonomous {
      }
      	@Override
     	 public void update() {
-    			calcNext();
-    			doTransition();
-    			currentState = nextState;
-    			time--;
-    		}
+     		switch (currentState) {
+			case START:
+				currentState = motionProfileStates.RUNPROFILE;
+				left.configureEncoder(drive.getLeftPositionTicks(), Constants.kDriveEncoderCodesPerRev , Constants.kPulleyDiameter / 12);
+				right.configureEncoder(drive.getRightPositionTicks(), Constants.kDriveEncoderCodesPerRev , Constants.kPulleyDiameter / 12);
+				left.configurePIDVA(1.0, 0.0, 0.0, 1 / Constants.kMaxVelocity, 0);
+				break;
+				
+			case RUNPROFILE:
+				double l = left.calculate(drive.getLeftPositionTicks());
+				double r = right.calculate(drive.getRightPositionTicks());
+				double gyroHeading = drive.getAngle();
+				double desiredHeading = Pathfinder.r2d(left.getHeading());
+				double angleDifference = Pathfinder.boundHalfDegrees(desiredHeading - gyroHeading);
+				double turn = Constants.kMotionProfileGyro_kP  * angleDifference;
+				
+				drive.setDesiredSpeed(l - turn, r + turn);
+				if (left.isFinished() && right.isFinished())  {
+					currentState = motionProfileStates.DONE;
+				}
+				break;
+			case DONE:
+				break;
+ 		}
+    		time--;
+    }
+     	
      	@Override
     		public void reset() {
     			currentState = motionProfileStates.START;
     		}
-    		
-
-     	@Override
-    		public void calcNext() {
-    			nextState = currentState;
-    			switch (currentState) {
-    				case START:
-    					nextState = motionProfileStates.RUNPROFILE;
-    					break;
-    					
-    				case RUNPROFILE:
-    					double l = left.calculate(drive.getLeftPosition());
-    					double r = right.calculate(drive.getRightPosition());
-    					double gyroHeading = drive.getAngle();
-    					double desiredHeading = Pathfinder.r2d(left.getHeading());
-    					double angleDifference = Pathfinder.boundHalfDegrees(desiredHeading - gyroHeading);
-    					double turn = Constants.kMotionProfileGyro_kP  * angleDifference;
-    					
-    					drive.setDesiredSpeed(l - turn, r + turn);
-    					if (left.isFinished() && right.isFinished())  {
-    						nextState = motionProfileStates.DONE;
-    					}
-    					break;
-    				case DONE:
-    					break;
-    			}
-    			
-    			
-    		}
-     	@Override
-    		public void doTransition() {
-    			if (currentState == motionProfileStates.START && nextState == motionProfileStates.RUNPROFILE) {	
-    				left.configureEncoder(drive.getLeftPosition(), 2048 , Constants.kPulleyDiameter / 12);
-    				right.configureEncoder(drive.getRightPosition(), 2048 , Constants.kPulleyDiameter / 12);
-    				left.configurePIDVA(1.0, 0.0, 0.0, 1 / Constants.kMaxVelocity, 0);
-    			
-    				}
-    			if (currentState == motionProfileStates.RUNPROFILE && nextState == motionProfileStates.DONE) {
-    				
-  
-    		}
-    	 
-     }
-
 }
