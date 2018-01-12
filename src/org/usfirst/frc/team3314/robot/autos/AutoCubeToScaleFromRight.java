e68e362bed6696fdc0f338587f4dff940942436a
crezeshox <crezeshox@gmail.com>
2018-01-11 20:02:39 -0500

    new classes, small fix

---
+++ src/org/usfirst/frc/team3314/robot/autos/AutoCubeToScaleFromRight.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team3314.robot.autos;
+
+public class AutoCubeToScaleFromRight implements Autonomous {
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/org/usfirst/frc/team3314/robot/autos/Autonomous.java
+++ src/org/usfirst/frc/team3314/robot/autos/Autonomous.java
@@ -2,10 +2,6 @@ package org.usfirst.frc.team3314.robot.autos;
 
 public interface Autonomous {
 	
-		enum State {
-			
-		};
-	
 		public void reset();
 	
 		public void update();
